dc0f01338b82144b12f20b4f311fce03b94614cf
Naoto Hori <hori.naoto@gmail.com>
2021-06-22 14:29:52 +0100

    Organize job types. DCD job is moved from main

---
+++ src/job_dcd.F90
@@ -0,0 +1,65 @@
+subroutine job_dcd()
+
+   use, intrinsic :: iso_fortran_env, Only : iostat_end
+   use const
+   use const_idx, only : ENE
+   use var_top, only : nmp, nchains, nmp_chain, seq, imp_chain, pbc_box, pbc_box_half, flg_pbc
+   use var_state, only : xyz, energies
+   use var_io, only : hdl_dcd, hdl_out, cfile_dcd_in, cfile_out
+   use dcd, only : file_dcd, DCD_OPEN_MODE
+
+   implicit none
+
+   integer :: i, nframe, istat, nmp_dcd
+   type(file_dcd) :: fdcd
+
+   fdcd = file_dcd(hdl_dcd, cfile_dcd_in, DCD_OPEN_MODE%READ)
+
+   call fdcd%read_header()
+
+   call fdcd%read_nmp(nmp_dcd, istat)
+
+   if (fdcd%box(1) > 0.0) then
+      flg_pbc = .True.
+      pbc_box(:) = fdcd%box(:)
+      pbc_box_half = 0.5 * pbc_box(:)
+      write(*,*) '#Box: ', pbc_box
+   else
+      flg_pbc = .False.
+      write(*,*) '#No PBC'
+   endif
+
+   if (nmp_dcd /= nmp) then
+      write(*,*) "nmp = ", nmp, " is inconsistent with nmp_chain = ", nmp_chain, " and nchains = ", nchains
+      stop (2) 
+   endif
+
+   allocate(xyz(3, nmp))
+
+   open(hdl_out, file = cfile_out, status = 'replace', action = 'write', form='formatted')
+   write(hdl_out, '(a)') '#(1)nframe  (2)Etotal  (3)Ebond   (4)Eangl   (5)Ebp   (6)Eele'
+
+   nframe = 0
+   do
+      call fdcd%read_onestep(nmp, xyz, istat)
+      if (istat == iostat_end) exit
+      nframe = nframe + 1
+
+      call energy()
+      
+      write(hdl_out, '(i10, 5(1x,g15.8))') nframe, (energies(i), i=0,ENE%MAX)
+
+      !! Debug
+      !if (nframe == 10) then
+      !   exit
+      !endif
+
+   enddo
+
+   write(*,*) '#nframe:', nframe
+
+   call fdcd%close()
+
+   close(hdl_out)
+
+endsubroutine job_dcd
--- src/main.F90
+++ src/main.F90
@@ -5,21 +5,15 @@ program sis
    use const_phys, only : BOLTZ_KCAL_MOL
    use const_idx, only : ENE, SEQT, JOBT
    use var_top, only : nmp, nchains, nmp_chain, seq, imp_chain, pbc_box, pbc_box_half, flg_pbc, ichain_mp, nrepeat
-   use var_state, only : xyz, energies, tempK, kT, job
-   use var_io, only : hdl_dcd, hdl_out, flg_out_bp, flg_out_bpe, hdl_bp, hdl_bpe, KIND_OUT_BP, KIND_OUT_BPE, &
+   use var_state, only : xyz, tempK, kT, job
+   use var_io, only : flg_out_bp, flg_out_bpe, hdl_bp, hdl_bpe, KIND_OUT_BP, KIND_OUT_BPE, &
                       cfile_ff, cfile_dcd_in, cfile_prefix, cfile_out, cfile_bp
-   use dcd, only : file_dcd, DCD_OPEN_MODE
 
    implicit none
 
    character(CHAR_FILE_PATH) cfile_inp
 
-   type(file_dcd) :: fdcd
-
    integer :: i, j, imp
-   integer :: istat
-   integer :: nframe
-   integer :: nmp_dcd
 
    character(500) :: cline
    logical :: stat
@@ -77,10 +71,7 @@ program sis
    endif
 
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-   ! Temporary hard-code the system setup
 
-   !nrepeat = 47
-   !nchains =  1
    allocate(nmp_chain(nchains))
    nmp_chain(:) = 3 * nrepeat
    nmp = sum(nmp_chain)
@@ -119,54 +110,17 @@ program sis
    call list_bp()
    !call read_sisinfo(cfile_sis)
 
-   fdcd = file_dcd(hdl_dcd, cfile_dcd_in, DCD_OPEN_MODE%READ)
+   if (job == JOBT%DCD) then
+      call job_dcd()
 
-   call fdcd%read_header()
+!   else if (job == JOBT%CHECK_FORCE) then
+!      call job_check_force()
+!
+!   else if (job == JOBT%MD) then
+!      call job_md()
 
-   call fdcd%read_nmp(nmp_dcd, istat)
-
-   if (fdcd%box(1) > 0.0) then
-      flg_pbc = .True.
-      pbc_box(:) = fdcd%box(:)
-      pbc_box_half = 0.5 * pbc_box(:)
-      write(*,*) '#Box: ', pbc_box
-   else
-      flg_pbc = .False.
-      write(*,*) '#No PBC'
    endif
 
-   if (nmp_dcd /= nmp) then
-      write(*,*) "nmp = ", nmp, " is inconsistent with nmp_chain = ", nmp_chain, " and nchains = ", nchains
-      stop (2) 
-   endif
-
-   allocate(xyz(3, nmp))
-
-   open(hdl_out, file = cfile_out, status = 'replace', action = 'write', form='formatted')
-   write(hdl_out, '(a)') '#(1)nframe  (2)Etotal  (3)Ebond   (4)Eangl   (5)Ebp   (6)Eele'
-
-   nframe = 0
-   do
-      call fdcd%read_onestep(nmp, xyz, istat)
-      if (istat == iostat_end) exit
-      nframe = nframe + 1
-
-      call energy()
-      
-      write(hdl_out, '(i10, 5(1x,g15.8))') nframe, (energies(i), i=0,ENE%MAX)
-
-      !! Debug
-      !if (nframe == 10) then
-      !   exit
-      !endif
-
-   enddo
-
-   write(*,*) '#nframe:', nframe
-
-   call fdcd%close()
-
-   close(hdl_out)
    if (flg_out_bp) then
       close(hdl_bp)
    endif
