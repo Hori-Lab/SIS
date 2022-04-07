05003dd86bcc3bca79d6dd29d6e6036047025ecc
Naoto Hori <hori.naoto@gmail.com>
2022-04-07 01:32:54 +0100

    minor format fix in const_phys

--- src/const_phys.F90
+++ src/const_phys.F90
@@ -1,20 +1,27 @@
 module const_phys
 
-  use const
+   use const
 
-  real(PREC), parameter :: PI = 3.141592653589793238462643383279
-  real(PREC), parameter :: EPS0 = 8.8541878128             ! Electric constant [F/m]
-  real(PREC), parameter :: ELE  = 1.602176634e-19_PREC     ! Elementary charge [C]
-  real(PREC), parameter :: BOLTZ_J = 1.380649e-23_PREC     ! Boltzmann constant [J/K]
-  real(PREC), parameter :: N_AVO   = 6.02214076e23_PREC    ! Avogadro constant [/mol]
-  real(PREC), parameter :: KCAL2JOUL = 4184.0              ! (kcal -> J)  [J/kcal]
+   real(PREC), parameter :: PI = 3.141592653589793238462643383279
+   real(PREC), parameter :: EPS0 = 8.8541878128             ! Electric constant [F/m]
+   real(PREC), parameter :: ELE  = 1.602176634e-19_PREC     ! Elementary charge [C]
+   real(PREC), parameter :: BOLTZ_J = 1.380649e-23_PREC     ! Boltzmann constant [J/K]
+   real(PREC), parameter :: N_AVO   = 6.02214076e23_PREC    ! Avogadro constant [/mol]
+   real(PREC), parameter :: KCAL2JOUL = 4184.0              ! (kcal -> J)  [J/kcal]
 
-  real(PREC), parameter :: JOUL2KCAL = 1.0/KCAL2JOUL   !< (J -> kcal)  [kcal/J]
-  real(PREC), parameter :: JOUL2KCAL_MOL  = JOUL2KCAL * N_AVO  !< (J -> kcal/mol)
-  real(PREC), parameter :: BOLTZ_KCAL_MOL = BOLTZ_J * JOUL2KCAL_MOL   !< Boltzmann constant [kcal/mol/K]
+   real(PREC), parameter :: JOUL2KCAL = 1.0/KCAL2JOUL   !< (J -> kcal)  [kcal/J]
+   real(PREC), parameter :: JOUL2KCAL_MOL  = JOUL2KCAL * N_AVO  !< (J -> kcal/mol)
+   real(PREC), parameter :: BOLTZ_KCAL_MOL = BOLTZ_J * JOUL2KCAL_MOL   !< Boltzmann constant [kcal/mol/K]
 
-  real(PREC), parameter :: INVALID_JUDGE = 1.0e30_PREC
-  real(PREC), parameter :: INVALID_VALUE = 1.0e31_PREC
-  real(PREC), parameter :: ZERO_JUDGE    = 1.0e-6_PREC
+   real(PREC), parameter :: INVALID_JUDGE = 1.0e30_PREC
+   real(PREC), parameter :: INVALID_VALUE = 1.0e31_PREC
+   real(PREC), parameter :: ZERO_JUDGE    = 1.0e-6_PREC
+
+contains
+
+   real(PREC) function J2kcalmol(j)
+      real(PREC), intent(in) :: j
+      J2kcalmol = j * JOUL2KCAL_MOL
+   endfunction J2kcalmol
 
 endmodule const_phys
