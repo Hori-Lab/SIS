704d23732263bbefc7b4d98cdfdbde0eaa254034
Naoto Hori <hori.naoto@gmail.com>
2022-06-02 22:40:08 +0100

    Update build.sh for augusta

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
+set -eu
+
 mkdir -p build
 cd build
 
 hostname=`hostname`
 
 if [[ ${hostname:8:9} == 'sulis.hpc' ]]; then
+    FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
+elif [[ ${hostname:13:24} == 'augusta.nottingham.ac.uk' ]]; then
     FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
 else
     cmake .. -DCMAKE_BUILD_TYPE=Release
