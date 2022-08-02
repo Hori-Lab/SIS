a99f8a16da23b164b336c56dad8bdc55754f9722
JamesRNAsim <98846152+JamesRNAsim@users.noreply.github.com>
2022-08-02 13:32:54 +0100

    Update read_sisinfo.F90

--- src/read_sisinfo.F90
+++ src/read_sisinfo.F90
@@ -1,7 +1,7 @@
 subroutine read_sisinfo(cfilepath)
       
    use const
-   use var_potential, only : nbond, bond_mp, bond_para, nangl, angl_mp, angl_para
+   use var_potential, only : nbond, bond_mp, bond_para, nangl, angl_mp, angl_para, ndihedral, dihedral_mp, dihedral_para
    use var_io, only : iopen_hdl
   
    implicit none
@@ -12,8 +12,8 @@ subroutine read_sisinfo(cfilepath)
    integer :: istat
 
    integer :: hdl
-   integer :: s, imp1, imp2, imp3, iunit1, iunit2, iunit3, imp1_unit, imp2_unit, imp3_unit
-   integer :: ibd, iangl
+   integer :: s, imp1, imp2, imp3, imp4, iunit1, iunit2, iunit3, imp1_unit, imp2_unit, imp3_unit, iunit4, imp4_unit
+   integer :: ibd, iangl, idihedral
    real(PREC) :: k, l
 
    iopen_hdl = iopen_hdl + 1
@@ -24,6 +24,7 @@ subroutine read_sisinfo(cfilepath)
    ! Count the nubmer of interactions
    ibd = 0
    iangl = 0
+   idihedral = 0
    do
       read (hdl, '(a)', iostat = istat) cline
       if(istat < 0) then
@@ -43,21 +44,29 @@ subroutine read_sisinfo(cfilepath)
       if(cline(1:4) == 'angl') then
          iangl = iangl + 1
       endif
+      
+      !Dihedral
+      if(cline(1:4) == 'Dihe') then
+         idihedral = idihedral + 1
+      endif
    enddo
 
    nbond = ibd
    nangl = iangl
-
+   ndihedral = idihedral
    ! Allocate
    allocate(bond_mp(2, nbond))
    allocate(bond_para(2, nbond))
    allocate(angl_mp(3, nangl))
    allocate(angl_para(2, nangl))
+   allocate(dihedral_mp(3, ndihedral))
+   allocate(dihedral_para(2, ndihedral))
 
    rewind(hdl)
 
    ibd = 0
    iangl = 0
+   idihedral = 0
    do
       read (hdl, '(a)', iostat = istat) cline
       if(istat < 0) then
@@ -106,6 +115,29 @@ subroutine read_sisinfo(cfilepath)
 
          angl_para(1, iangl) = k
          angl_para(2, iangl) = l
+
+      endif
+      !--------------------------------------------------------------------------------
+      ! dihedral
+      else if(cline(1:4) == 'Dihe') then
+         read(cline(5:), *) s, imp1, imp2, imp3, imp4, iunit1, imp1_unit, iunit2, imp2_unit, iunit3, imp3_unit, iunit4, imp4_unit, k, l
+          
+         idihedral = idihedral + 1
+
+         if (imp1 < imp4) then
+            dihedral_mp(1, idihedral) = imp1
+            dihedral_mp(2, idihedral) = imp2
+            dihedral_mp(3, idihedral) = imp3
+            dihedral_mp(4, idihedral) = imp4
+         else
+            dihedral_mp(1, idihedral) = imp4
+            dihedral_mp(2, idihedral) = imp3
+            dihedral_mp(3, idihedral) = imp2
+            dihedral_mp(4, idihedral) = imp1
+         endif
+
+         dihedral_para(1, idihedral) = k
+         dihedral_para(2, idihedral) = l
 
       endif
 
