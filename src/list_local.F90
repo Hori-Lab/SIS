d3e01182b4ecc46cdfe3c65461716428a7e51c34
JamesRNAsim <98846152+JamesRNAsim@users.noreply.github.com>
2022-08-02 14:50:05 +0100

    Update list_local.F90

--- src/list_local.F90
+++ src/list_local.F90
@@ -2,13 +2,13 @@ subroutine list_local()
 
    use const
    use var_top, only : nmp_chain, imp_chain, nchains
-   use var_potential, only : nbond, bond_mp, nangl, angl_mp
+   use var_potential, only : nbond, bond_mp, nangl, angl_mp, ndihedral, dihedral_mp
 
    implicit none
   
    integer :: ichain
    integer :: n, i, imp
-   integer :: ibond, iangl
+   integer :: ibond, iangl, idihedral
 
    print '(a)', 'Making lists of local interactions.'
 
@@ -16,7 +16,7 @@ subroutine list_local()
   
       ibond = 0
       iangl = 0
-
+      idihedral = 0
        do ichain = 1, nchains
     
           do i = 1, nmp_chain(ichain)-1
@@ -39,8 +39,13 @@ subroutine list_local()
                    angl_mp(2, iangl) = imp + 1
                    angl_mp(3, iangl) = imp + 2
                 endif
-
-             endif
+                !CHECK HERE - -1, -2 or -3
+                if (i /= nmp_chain(ichain) - 1) then
+                   dihedral_mp(1, idihedral) = imp
+                   dihedral_mp(2, idihedral) = imp + 1
+                   dihedral_mp(3, idihedral) = imp + 2
+                   dihedral_mp(4, idihedral) = imp + 3
+                endif
 
           enddo
     
@@ -49,13 +54,16 @@ subroutine list_local()
        if (n == 1) then
           nbond = ibond
           nangl = iangl
+          ndihedral = idihedral
           allocate(bond_mp(2, nbond))
           allocate(angl_mp(3, nangl))
+          allocate(dihedral_mp(4, ndihedral))
        endif
    enddo
 
    print '(a,i10)', "# Number of bonds: ", nbond
    print '(a,i10)', "# Number of angles: ", nangl
+   print '(a,i10)', "# Number of dihedrals: ", ndihedral
    print *
 
 end subroutine list_local
