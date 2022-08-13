bc55ab6347e7aa09b6a987f01effdcb7654d43f3
Naoto Hori <hori.naoto@gmail.com>
2022-08-13 21:00:30 +0100

    Add missing files in the previous commit

---
+++ src/energy_dih_exp.F90
@@ -0,0 +1,42 @@
+subroutine energy_dih_exp(Edih)
+
+   use const
+   use pbc, only : pbc_vec_d
+   use var_state, only : xyz
+   use var_potential, only : ndih, dih_mp, dih_k, dih_w, dih_p0
+
+   implicit none
+
+   real(PREC), intent(inout) :: Edih
+
+   integer :: idih, imp1, imp2, imp3, imp4
+   real(PREC) :: dih, coef
+   real(PREC) :: c1(3), c2(3)
+   real(PREC) :: v12(3), v23(3), v34(3)
+
+   coef = - 0.5_PREC * dih_w
+
+   do idih = 1, ndih
+      imp1 = dih_mp(1, idih)
+      imp2 = dih_mp(2, idih)
+      imp3 = dih_mp(3, idih)
+      imp4 = dih_mp(4, idih)
+
+      v12(:)= pbc_vec_d(xyz(:, imp2), xyz(:, imp1))
+      v23(:)= pbc_vec_d(xyz(:, imp3), xyz(:, imp2))
+      v34(:)= pbc_vec_d(xyz(:, imp4), xyz(:, imp3))
+
+      c1(1) = v12(2)*v23(3) - v12(3)*v23(2)
+      c1(2) = v12(3)*v23(1) - v12(1)*v23(3)
+      c1(3) = v12(1)*v23(2) - v12(2)*v23(1)
+
+      c2(1) = v23(2)*v34(3) - v23(3)*v34(2)
+      c2(2) = v23(3)*v34(1) - v23(1)*v34(3)
+      c2(3) = v23(1)*v34(2) - v23(2)*v34(1)
+
+      dih = atan2(norm2(v23) * dot_product(v12, c2), dot_product(c1, c2))
+
+      Edih = Edih - dih_k * exp(coef * (dih - dih_p0)**2)
+    end do
+
+end subroutine energy_dih_exp
---
+++ src/force_dih_exp.F90
@@ -0,0 +1,65 @@
+subroutine force_dih_exp(forces)
+
+   use const
+   use pbc, only : pbc_vec_d
+   use var_state, only : xyz
+   use var_top, only : nmp
+   use var_potential, only : ndih, dih_mp, dih_k, dih_w, dih_p0
+
+   implicit none
+
+   real(PREC), intent(inout) :: forces(3, nmp)
+
+   integer :: idih, imp1, imp2, imp3, imp4
+   real(PREC) :: dih, akj, akj2, pre, dvijvkj_akj2, dvklvkj_akj2
+   real(PREC) :: coef1, coef2
+   real(PREC) :: m(3), n(3)
+   real(PREC) :: vij(3), vkj(3), vkl(3), fi(3), fj(3), fk(3), fl(3)
+
+   coef1 = dih_k * dih_w
+   coef2 = - 0.5_PREC * dih_w
+
+   !$omp   do private(imp1, imp2, imp3, imp4, vij, vkj, vkl, m, n, &
+   !$omp&             akj2, akj, dih, pre, fi, fj, fk, fl, dvijvkj_akj2, dvklvkj_akj2)
+   do idih = 1, ndih
+      imp1 = dih_mp(1, idih)
+      imp2 = dih_mp(2, idih)
+      imp3 = dih_mp(3, idih)
+      imp4 = dih_mp(4, idih)
+
+      vij(:) = pbc_vec_d(xyz(:, imp1), xyz(:, imp2))
+      vkj(:) = pbc_vec_d(xyz(:, imp3), xyz(:, imp2))
+      vkl(:) = pbc_vec_d(xyz(:, imp3), xyz(:, imp4))
+
+      ! m = rij x rkj
+      m(1) = vij(2)*vkj(3) - vij(3)*vkj(2)
+      m(2) = vij(3)*vkj(1) - vij(1)*vkj(3)
+      m(3) = vij(1)*vkj(2) - vij(2)*vkj(1)
+
+      ! n = rkj x rkl
+      n(1) = vkj(2)*vkl(3) - vkj(3)*vkl(2)
+      n(2) = vkj(3)*vkl(1) - vkj(1)*vkl(3)
+      n(3) = vkj(1)*vkl(2) - vkj(2)*vkl(1)
+
+      akj2 = dot_product(vkj, vkj)
+      akj = sqrt(akj2)
+
+      dih = atan2(akj * dot_product(vij, n), dot_product(m, n))
+
+      pre = coef1 * (dih - dih_p0) * exp( coef2 * (dih - dih_p0)**2) * akj
+      fi(:) = -pre / dot_product(m,m) * m(:)
+      fl(:) = +pre / dot_product(n,n) * n(:)
+
+      dvijvkj_akj2 = dot_product(vij, vkj) / akj2
+      dvklvkj_akj2 = dot_product(vkl, vkj) / akj2
+      fj(:) = (-1.0_PREC + dvijvkj_akj2) * fi(:) + (          - dvklvkj_akj2) * fl(:)
+      fk(:) = (          - dvijvkj_akj2) * fi(:) + (-1.0_PREC + dvklvkj_akj2) * fl(:)
+
+      forces(:, imp1) = forces(:, imp1) + fi(:)
+      forces(:, imp2) = forces(:, imp2) + fj(:)
+      forces(:, imp3) = forces(:, imp3) + fk(:) 
+      forces(:, imp4) = forces(:, imp4) + fl(:)
+   end do 
+   !$omp end do nowait
+
+end subroutine force_dih_exp
