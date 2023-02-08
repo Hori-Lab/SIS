209951a7974975c971e335d0b51f368d9e152f13
Naoto Hori <hori.naoto@gmail.com>
2023-02-08 23:07:24 +0000

    Revert previous changes to build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ cd build
 hostname=`hostname`
 
 if [[ ${hostname:8:9} == 'sulis.hpc' ]]; then
-    #FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
-    FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Debug
+    FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
 elif [[ ${hostname:13:24} == 'augusta.nottingham.ac.uk' ]]; then
     FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
 else
