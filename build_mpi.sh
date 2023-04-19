2a1214b1952259f4286e8be965196b8fa7566f84
Naoto Hori <hori.naoto@gmail.com>
2023-04-19 12:54:23 +0100

    Edit build_mpi.sh for cosma

--- build_mpi.sh
+++ build_mpi.sh
@@ -13,6 +13,8 @@ elif [[ ${hostname:13:24} == 'augusta.nottingham.ac.uk' ]]; then
     #FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
     echo 'Edit build_mpi.sh!'
     exit 2
+elif [[ ${hostname:12:5} == 'cosma' ]]; then
+    FC=mpiifort cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_MPI=ON
 else
     FC=mpifort cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_MPI=ON
 fi
