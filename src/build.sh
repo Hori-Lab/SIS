c5acd46dc5021d0cfd2303a02d490e0dbb8f9510
meoy9 <vuhuong@live.com>
2023-08-21 22:17:09 +0100

    correct out format in dcd runs

--- src/build.sh
+++ src/build.sh
@@ -4,14 +4,16 @@ mkdir -p ../build && cd ../build
 #cmake -DCMAKE_BUILD_TYPE=Release ..
 
 # Fortran debug
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake ..
+#cmake -DCMAKE_BUILD_TYPE=Release ..
+#cmake -DCMAKE_BUILD_TYPE=Debug ..
 #cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_Fortran_FLAGS="-O0 -g -fbacktrace -ffpe-trap=zero,overflow,denormal,underflow -Wall -ffree-line-length-none -fbounds-check -fmax-errors=5" ..
 #cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_Fortran_FLAGS="-O0 -g -fbacktrace -ffpe-trap=zero,overflow,denormal,underflow -Wall -ffree-line-length-none -fbounds-check -fmax-errors=5 -D_HTN_CONSISTENT" ..
 
 #cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_FLAGS="-heap-arrays" ..
 #cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_Fortran_FLAGS="-O0 -g -traceback -check all -heap-arrays" ..
 
-make
+make -j12
 #make -j12 VERBOSE=1
 
 cd ../src
