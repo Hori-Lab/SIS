882ca26f6ecc337eccb06a2f2532a564a320ee62
Naoto Hori <hori.naoto@gmail.com>
2024-01-13 02:12:25 +0000

    Add compile option to disable IEEE_HALT in write_bp because Cray doen't like it

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ hostnameall=`hostname -A`
 
 if [[ ${hostnameall:(-14):(-1)} == 'archer2.ac.uk' ]]; then
     #FC=ftn cmake .. -DCMAKE_BUILD_TYPE=Release -DMT_USE_NORANGECHECK_INSTEAD_OF_ALLOWINVALIDBOZ=ON
-    FC=ftn cmake .. -DCMAKE_BUILD_TYPE=Release -DMT_USE_ALLOWINVALIDBOZ=OFF
+    FC=ftn cmake .. -DCMAKE_BUILD_TYPE=Release -DMT_USE_ALLOWINVALIDBOZ=OFF -DBP_HALT_IEEE_EXCEPTIONS=OFF
 elif [[ ${hostname:8:9} == 'sulis.hpc' ]]; then
     FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
 elif [[ ${hostname:13:24} == 'augusta.nottingham.ac.uk' ]]; then
