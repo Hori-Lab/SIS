d1c6018e32922cbe8135211ffc64aac5ab935812
Naoto Hori <hori.naoto@gmail.com>
2024-01-13 01:49:56 +0000

    Update cmake set up for archer2 cray

--- build.sh
+++ build.sh
@@ -4,21 +4,26 @@ mkdir -p build
 cd build
 
 hostname=`hostname`
+hostnameall=`hostname -A`
 
-if [[ ${hostname:8:9} == 'sulis.hpc' ]]; then
+if [[ ${hostnameall:(-14):(-1)} == 'archer2.ac.uk' ]]; then
+    #FC=ftn cmake .. -DCMAKE_BUILD_TYPE=Release -DMT_USE_NORANGECHECK_INSTEAD_OF_ALLOWINVALIDBOZ=ON
+    FC=ftn cmake .. -DCMAKE_BUILD_TYPE=Release -DMT_USE_ALLOWINVALIDBOZ=OFF
+elif [[ ${hostname:8:9} == 'sulis.hpc' ]]; then
     FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
 elif [[ ${hostname:13:24} == 'augusta.nottingham.ac.uk' ]]; then
     FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
 elif [[ ${hostname:12:5} == 'cosma' ]]; then
     FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Release
 elif [[ ${hostname:(-22)} == 'pharm.nottingham.ac.uk' ]]; then
-    FC=gfortran cmake .. -DCMAKE_BUILD_TYPE=Release -DMT_USE_NORANGECHECK_INSTEAD_OF_ALLOWINVALIDBOZ=ON
+    FC=gfortran cmake .. -DCMAKE_BUILD_TYPE=Release -DMT_USE_NORANGECHECK=ON -DMT_USE_ALLOWINVALIDBOZ=OFF
 else
     #FC=ifort cmake .. -DCMAKE_BUILD_TYPE=Debug
     FC=gfortran cmake .. -DCMAKE_BUILD_TYPE=Release
 fi
 
 make -j8
+#make VERBOSE=1
 
 cd ../
 ln -sf build/sis
