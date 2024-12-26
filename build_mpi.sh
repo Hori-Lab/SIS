954afbc81abc85d555aede2c6ea0a99f763b05cd
MEOY9 <vuhuong@live.com>
2024-12-26 21:43:17 +0000

    build sis_mpi on Archer2

--- build_mpi.sh
+++ build_mpi.sh
@@ -12,7 +12,9 @@ else
 fi
 
 if [[ ${hostnameall:(-14):(-1)} == 'archer2.ac.uk' ]]; then
-    FC=ftn cmake .. -DCMAKE_BUILD_TYPE=Release -DMT_USE_ALLOWINVALIDBOZ=OFF -DBP_HALT_IEEE_EXCEPTIONS=OFF -DBUILD_MPI=ON
+     FC=mpifort cmake .. -DCMAKE_BUILD_TYPE=Release -DALLOW_ARGUMENT_MISMATCH=ON -DBUILD_MPI=ON
+#if [[ ${hostnameall:(-14):(-1)} == 'archer2.ac.uk' ]]; then
+#    FC=ftn cmake .. -DCMAKE_BUILD_TYPE=Release -DMT_USE_ALLOWINVALIDBOZ=OFF -DBP_HALT_IEEE_EXCEPTIONS=OFF -DBUILD_MPI=ON
 elif [[ ${hostname:8:9} == 'sulis.hpc' ]]; then
     FC=mpiifort cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_MPI=ON
 elif [[ ${hostname:13:24} == 'augusta.nottingham.ac.uk' ]]; then
@@ -20,8 +22,6 @@ elif [[ ${hostname:13:24} == 'augusta.nottingham.ac.uk' ]]; then
     echo 'Edit build_mpi.sh!'
     exit 2
 elif [[ ${hostname:12:5} == 'cosma' ]]; then
-    FC=mpiifort cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_MPI=ON
-elif [[ ${hostname:(-20)} == 'ada.nottingham.ac.uk' ]]; then
     FC=mpiifort cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_MPI=ON
 else
     FC=mpifort cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_MPI=ON
